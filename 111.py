a14b14576befef984079e1231d3e9884c122a844
12345567789Aa <bajysovn7@gmail.com>
2022-09-20 17:43:19 +0600

    first commit

---
+++ 111.py
@@ -0,0 +1,1889 @@
+# it_company=("google","amazon","fecebook","instagram","Telegram")
+# print(it_company[1:4:2])
+# print(it_company[::-1]
+# it_company=tuple 
+# print(it_company)
+# ist_it_company=list(it_company)
+# print(ist_it_company )
+# ist_it_company.append("Space X")
+# print(ist_it_company )
+# it_company = tuple(ist_it_company)
+# print (it_company)
+#  a,b,c,d,=("лето",'зима','осень','весна')
+
+# # num1,num2,num3=10,40,100
+# # print(num1)
+# # print(num2)
+# # print(num1)
+
+
+# # num1="first"
+# # num2="second"
+# # num1, num2,num3,num1
+# # print(num2) 
+# # print(num1)
+ 
+ 
+# # number =88823234
+# # name="Dima"
+# # print("Name"+name+str(number))
+# # print(f"Name(name){number}")
+# # задача1 
+# # it_company=("Google","Amazon","microsoft")
+# # print(it_company[1:3:2])
+# # print(it_company[::-1])
+# # print(it_company)
+# # ist_it_company=list(it_company)
+# # print(ist_it_company)
+# # ist_it_company.TESLA
+# # print(ist_it_company)
+# # it_company=tuple(ist_it_company)
+# # print(it_company)
+# # задача2
+
+
+
+# num1 = 10
+# num2 = 20
+# if num1 == 10 or num2 ==10:
+#     print("first")
+# elif num1 == 20 or num2 ==20
+#     print("second")
+# else:
+#     print ()
+# number = int(input("введите число"))
+# if number % 2 ==0:
+#     print ("четное")
+# else:
+#     print ("нечетное")
+# number =int(input("вветите число:"))
+# print("четное")if number % 2 == 0 else print ("нечотное")
+   
+   
+   
+# num1 = int(input("введите первое число: "))
+# num2 = int(input("введите второе число: " ))
+# if num1 > num2
+
+
+# number =int(input("введите число:"))
+# res =("четное","нечотное")[number % 2 == 0]
+# print(res)
+
+
+# num1 =20
+# num2 = 400
+# num3 = 60
+# result ="Первое число"if num1 > num2 and num1 > num3 else "третье"
+# print(result)
+
+
+# задача1 
+# age = int(input("возраст:"))
+# if age > = 18 and age <= 50:
+#     print ("доступ разрешен")
+# elif age > 50
+#     print("вы уже старый")
+# else:
+#     print("иди домой ")
+    
+# for  i in range(1001):
+#     print(i)
+    
+# numbers =[2,3,4,5,9,11,12,103,104,101]
+# for num in numbers:
+#     if num % 2 == 0:
+#         print(num, "четный")
+# else:
+   
+#     print(num, "нечетный")
+# numbers =[2,3,4,5,6,7,8,9,10]
+# for num in numbers:
+#  if num % 2 == 0: 
+#         print(num,"нечетный")
+#  else:
+#      print(num,"четный")
+# numbers =(2,3,5,9,11,12,103,104,101,)
+# fori in numbers:
+#      print(1)
+
+# it = "IT RUN"
+# for n  in it:
+#     print(n)
+
+# for n in range(0,10,2):
+#     print(n)
+
+# for i in range(1,100):
+#     if i == 60:
+#         print ("STOP")
+#         break
+#     print(i)
+
+# numbers = [2,3,4,100,5,9,11,12,103,104,101]  
+# for i in numbers:
+#     print(i)
+#     if i == 100:
+#         print ("STOP ITERATION")
+#         break
+
+# num1 = 10 
+# num2 =30 
+# while num1 < num2:
+#    num1 += 10:
+#    print(num1)
+
+# num = 10 
+# while True:
+#     num += 1 
+#     print(num)
+#     if num == 10000:
+#         break
+
+# import time 
+# n = 0
+# while True:
+#     n += 10
+#     print(f"Hack {n}%")
+#     time.sleep(0.5)
+#     if n == 100:
+#         print("Hacked")
+
+# import random 
+
+# random_num =random .randint(1, 10)
+# n = 0
+# while True:
+#     user_num =int(input("введите числос 1 до 10:"))
+#     n += 1 
+#     if n == 3:
+#         print("у вас закончилиись попытки")
+#         print (f"случайное число:{random_num}") 
+#         print("Вы проиграли")
+#         break
+#     if random_num == user_num :
+#         print("Вы победили")
+#         break
+#     elif random_num != user_num:
+#         print(f"неправилное у вас {3 - n} попыток")
+#     else:
+#         print("написано же ввести цифры от 1 до 10")
+# # задача 1 
+# for i in range(5)
+#   print("0 , i")
+# #   задача 2 
+# lst = []
+# for i in range(1,101):
+#     lst.append(i)
+# print(lst)
+# задача3 
+# ist=[]
+# for  i in range["1,497"] 
+#       ist.(append)
+#       print(ist)
+  
+# try:
+#     print(10\0)
+# except ZeroDivisionError:
+#       print("деление на ноль")
+  
+# try:
+#         print(  10+ "10")
+# except TypeError:
+#       print("ошибка типа:неподдерживаемые типы операндов для +: 'int' и 'str'")
+# tur:  
+# number1 = int(input("введите первое число:"))
+# number2 =int(input("введите второе число:") )
+# except ValueError:
+#       print
+      
+# try:
+#       print( 100 / 0 )
+# except ZeroDivisionError:
+#       raise OSError("у вас ошибка в коде")
+# finally: 
+#       print("я все равно сработаю")
+
+# while True:
+      # try:
+      #       num1 = int(input("введите первое число: "))
+      #       operation= input("*,/,+,-: ")
+      #       num2 = int (input("введите второе число : "))
+      #       if operation == "+":
+      #             print(num1 +num2)
+      #       elif operation == "-":
+      #             print(num1-num2)
+      #       elif  operation == "*":
+      #             print(num1 * num2 ) 
+      #       elif operation =="/":
+      #             try:
+      #                   print(num1/num2)
+      #             except ZeroDivisionError:
+      #                   print("деление на ноль ")
+      #       else:
+      #             print("неправильная операция")
+      # except :ValueError
+        
+      # print("введите целое число")
+            
+# from unicodedata import name
+
+
+# student ={'name' : 'adilet', 'age' : 18} 
+# print(student['age'])  
+# student['age']
+# del student['age']
+# print(student)
+# print(student.get('age'))
+# student.setdefault('key','value')
+# student['hello']= 'world'
+# print(student)
+# student_dct =dict(name = 'kurmanbek', age =18)
+# print(student_dct)
+# print(student.pop('hello'))
+# car ={'name' : 'Lexus', 'year': 2022, 'colop':'Black'}
+# print(car.keys())
+# print(car.values())
+# print(car.items())
+# for key, value in car.items():
+#       print(key, value)
+
+# from tkinter.font import names
+
+
+# 1  a = [1,2,3,4,5]
+# 2  b = [2,3,4,5,6,]
+# 3  c = a + b
+# print(set(c))
+
+# names ={"Adilet","Kurmanbek","Sardor","Adilet"}
+# print(names)
+# numbers ={1,2,3,4,5,6,7,8,9,10}
+# print(numbers)
+
+# logic ={1,1.0,Ture}
+# print(logic)
+# strange_app = set('tiktok')
+# print(strange_app)
+# types ={1,2.0, False,"1",(10
+
+# from typing import overload
+
+
+# some_dict = {'key_one': 'val_one', 'key_two': 'val_two'}
+# some_set = set(some_dict)
+# print(some_set)
+ 
+# card_suit =['heart','diamond','clud','spade','spade']
+# suit_set =set(card_suit)
+# print(suit_set)
+
+# unbreakable_diamond =['jotaro','josuke',' koichi'] 
+# golden_wind =['jotaro','koichi','giorno']
+# overlap =set(unbreakable_diamond) & set (golden_wind)
+# print(overlap)
+
+# it_company  ={"Google","Tesla","Amazon"}
+# it_company.app("microsoft")
+# print(it_company)
+
+# it_company.remove("Google",)
+# print(it_company)
+
+# it_company.pop()
+# print
+
+# it_company  =["Google","Tesla","Amazon"]
+# it_company.append("Microsoft")
+# frzn_it = frozenset(it_company)
+# print(frzn_it)
+# # frzn_it.remove("Google")
+# print(frzn_it)
+
+# nums =[]
+# for num in range(1,101):
+#       nums.append(num)
+# print(nums)
+ 
+# nums = [num for num in range(1,101)]
+# print(nums)
+
+# nums =[]
+# for i in range(101):
+#       if i % 2 == 0:
+#             nums.append(i)
+# print(nums)
+
+# nums = [i for ige()
+
+# from xxlimited import new
+
+
+# petrol = { 
+#       'Ai 100':60,
+#       'Ai 95 ':55,
+#       'AI 92 ':50,
+#       'Ai 80' :40,
+#       'DT':  35
+# }
+
+# new_petrol ={}
+# for name,price in petrol.items():
+#       new_petrol.setdefault(name,price +16)
+# print(new_petrol)
+
+# new_petrol = {name:price +16 for name,price in petrol. items()}
+# print(new_petrol)
+
+# time_one = input("введите начало времени: ")
+# time_two = input("введите конец: ")
+# # 01234567
+    
+    
+# hours_one = int ()    # 10:00:50
+
+# from types import new_class
+
+
+# def short_word(word):
+#      word_split = word.split()
+#      new_lst =[]
+#      for i in word_split:
+#            new_lst.append(i.title() [0] )
+#             res = "".join(new_lst)
+#             print(res)
+
+#           short_word()
+
+# def count_word(word):
+#     word_split = word.split(",")    
+#     str_split = "". join(word_split)
+#     lower()
+#     word_split= str_split. split()
+#     for i in set(word_split):
+#         print(word_split.count(i),i)
+#         count_word( "Money, money, money, it’s always sunny,")
+
+# from turtle import back
+
+
+# def welcome(*names):
+#       for name in names: 
+#             print(name,"winner")
+            
+# welcome( "nurdoolot","islam", "IT","RUN")
+# welcome(1,2,3,)
+
+# def translate(**words): 
+#     for key, value in words.items()
+#         print(key, value)
+
+# translate(USA="США",Russia = "Россия",Kyrgyzstan ="кыргызстан")
+
+
+# def game(name,*points):
+#       print(name, points)
+# game("Player1",5,5,4,3,35,6,4)
+
+# def = { 30 ,50,78,90, 100,111,401,}
+      
+
+# new_def  ={}
+# for name,price in def.items():
+#       new_def .setdefault(name,price *2)
+# print(new_def )
+
+# new_def = {name:price +16 for name,price in def. items()}
+# print(new_def)
+
+# def add(num1 ,num2 ):
+#       print (num1 * num2 )
+
+# def sud(num1 , num2 ):
+#       print(num1 ,num2 )
+
+#  it = "IT-RUN "
+
+
+# def students(*names):
+#       print(names)
+
+import imp
+imp.add(20,40)
+imp.sud()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ main.py
@@ -0,0 +1,60 @@
+# def hello():
+#   print("Hello world")
+
+# def it(): 
+#   print("IT RUN")
+  
+# hello()
+# it()
+# def calc():
+#     num1 = int(input("введите первое число:"))
+#     num2 = int(input("введите второе число:"))
+#     print(num1 * num2)
+    
+# calc()
+
+# def calc(num1, num2): 
+#     print(num1 / num2)
+    
+# calc(50,2)
+
+# from statistics import _NumberT
+
+
+# def chet_nechet(number):
+#    if number % 2 == 0:
+#        print(number,"четный")
+#    else:
+#        print(number,"нечетны")
+
+# chet_nechet(2)
+# chet_nechet(15)
+# chet_nechet(27)
+# chet_nechet (28)
+
+# def for_range(number):
+#     for i in range(1, number):
+#         print(i)
+        
+# for_range(1001)
+
+# def for_range(number):
+#     for i in range(1,number):
+#         print(i)
+# for_range(111101)
+
+# def mult(num1 =  2 ,num2 = 3 ):
+#     print(num1 * num2)
+    
+# mult() 
+
+# def chat_bot():
+#     while True:
+#         word =  input ("напишите слова:")
+#         if word == "": 
+#             print("как классно, когда ты молчишь.Продолжай в том же духе!")
+#     chat_bot()    
+
+# задание2 
+# lower():кичинекей  
+# модули 
